555c66915c327014688d5b9a9234b76bb6476e34
Akshay Rasal <arasal@1-E-15.42heilbronn.de>
2023-12-13 18:08:28 +0100

    Added elements to send the data to api using the app url

--- data_read.py
+++ data_read.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import requests
 import sys
 
 # Read the Excel/CSV file into a pandas DataFrame
@@ -10,4 +11,12 @@ def read_data(file_path):
 		print(f"Error reading the file: {e}")
 		sys.exit(1)
 
-
+# Make a POST request to the API endpoint
+def send_to_api(api_url, json_data):
+	try:
+		response = requests.post(api_url, json=json_data)
+		response.raise_for_status()  # Raise an error for bad responses (4xx, 5xx)
+		print("Data sent successfully to the API.")
+	except requests.exceptions.RequestException as e:
+		print(f"Error sending data to the API: {e}")
+		sys.exit(1)
